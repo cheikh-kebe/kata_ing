51e4b7d857579c6cb70721e2fb39116508f72407
Cheikh <cheikh.kebe@sogeti.com>
2022-09-05 21:02:34 +0200

    styles: Header done with scss no responsive

---
+++ src/components/Header/Client_button/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './client_button.styles.scss';
+
+export const ClientButton = ({text}) => {
+  return (
+    <a href='#²' className='button--client'>
+      {text}
+    </a>
+  )
+}
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import './index.css';
+//styles
+import './index.scss';
+//assets
+import search from '../../assets/search.svg';
+import ing from '../../assets/ing-light.png'
+import { ClientButton } from './Client_button';
 
 export const Header = () => {
   return (
-    <div>Header
-      
-    </div>
+    <header className='header'>
+      <nav>
+        <ul className='nav'>
+          <li>
+            <a href="#" title="ING, Banque le ligne" className='logo'>
+              <img src={ing} alt="page d'accueil ING" />
+            </a> 
+          </li>
+          <li className='ing_search'>
+            <a href="#">
+              <img src={search} alt="search logo" />
+            </a>
+          </li>
+          <li className='projects'>
+            <a href="#">
+              Vos projets
+            </a>
+          </li>
+          <li className='help'>
+            <a href="#">
+              Besoin d'aide?
+            </a>
+          </li>
+          <li className='client'>
+            <ClientButton text='Espace client' />
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
