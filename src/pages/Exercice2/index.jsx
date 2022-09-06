ae149d53bc4c302ca0e1f772e4f1294125b5e417
Cheikh <cheikh.kebe@sogeti.com>
2022-09-06 21:24:46 +0200

    feat: fetch data with axios

--- src/pages/Exercice2/index.jsx
+++ src/pages/Exercice2/index.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
-
-const API_URL = 'https://api.github.com/users';
+import { useFetchData } from '../../app/Hooks/useFetchData';
+import './exercice2.styles.scss'
 
 export const Exercice2 = () => {
+  const { state, error } = useFetchData()
+  if (error) return <div>Oulala, il y a un problème</div>;
+  console.log(state.data);
   return (
-    <div>Exercice2</div>
+    <>
+      {state.data && state.data.map((user) => (
+        <div key={user.id} className="user">
+            <h1>Login: {user.login}</h1>
+            <img src={user.avatar_url} alt="" />
+              <p>link to : 
+                <a href={user.url}>
+                 {user.login}
+                </a>
+              </p>
+        </div>
+      ))}
+    </>
   )
 }
 
