ae0b432233ab9ceaba4938426901ec8068090047
Cheikh <cheikh.kebe@sogeti.com>
2022-09-06 21:27:17 +0200

    clean

--- src/pages/Exercice2/index.jsx
+++ src/pages/Exercice2/index.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
+//hooks
 import { useFetchData } from '../../app/Hooks/useFetchData';
+//styles
 import './exercice2.styles.scss'
 
 export const Exercice2 = () => {
-  const { state, error } = useFetchData()
+  const { state, error } = useFetchData();
   if (error) return <div>Oulala, il y a un problème</div>;
-  console.log(state.data);
+  
   return (
     <>
       {state.data && state.data.map((user) => (
         <div key={user.id} className="user">
-            <h1>Login: {user.login}</h1>
-            <img src={user.avatar_url} alt="" />
-              <p>link to : 
-                <a href={user.url}>
-                 {user.login}
-                </a>
-              </p>
+          <h1>Login: {user.login}</h1>
+          <img src={user.avatar_url} alt="" />
+          <p>link to :
+            <a href={user.url}>
+              {user.login}
+            </a>
+          </p>
         </div>
       ))}
     </>
